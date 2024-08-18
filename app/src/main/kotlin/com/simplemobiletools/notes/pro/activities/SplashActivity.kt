98814f23652bf01c010c08be42fad58874e44d3d
hojat <hojat72elect@gmail.com>
2024-08-18 12:48:55 -0400

    Merged BaseSplashActivity and SplashActivity into 1 class.

--- app/src/main/kotlin/com/simplemobiletools/notes/pro/activities/SplashActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/notes/pro/activities/SplashActivity.kt
@@ -2,11 +2,23 @@ package com.simplemobiletools.notes.pro.activities
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.simplemobiletools.notes.pro.R
+import com.simplemobiletools.notes.pro.extensions.baseConfig
+import com.simplemobiletools.notes.pro.extensions.checkAppIconColor
+import com.simplemobiletools.notes.pro.extensions.checkAppSideloading
+import com.simplemobiletools.notes.pro.extensions.getSharedTheme
+import com.simplemobiletools.notes.pro.extensions.isThankYouInstalled
+import com.simplemobiletools.notes.pro.extensions.isUsingSystemDarkTheme
+import com.simplemobiletools.notes.pro.extensions.showSideloadingDialog
 import com.simplemobiletools.notes.pro.helpers.OPEN_NOTE_ID
+import com.simplemobiletools.notes.pro.helpers.SIDELOADING_TRUE
+import com.simplemobiletools.notes.pro.helpers.SIDELOADING_UNCHECKED
 
 @SuppressLint("CustomSplashScreen")
-class SplashActivity : BaseSplashActivity() {
-    override fun initActivity() {
+class SplashActivity : AppCompatActivity() {
+    private fun initActivity() {
         if (intent.extras?.containsKey(OPEN_NOTE_ID) == true) {
             Intent(this, MainActivity::class.java).apply {
                 putExtra(OPEN_NOTE_ID, intent.getLongExtra(OPEN_NOTE_ID, -1L))
@@ -16,5 +28,54 @@ class SplashActivity : BaseSplashActivity() {
             startActivity(Intent(this, MainActivity::class.java))
         }
         finish()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (baseConfig.appSideloadingStatus == SIDELOADING_UNCHECKED) {
+            if (checkAppSideloading()) {
+                return
+            }
+        } else if (baseConfig.appSideloadingStatus == SIDELOADING_TRUE) {
+            showSideloadingDialog()
+            return
+        }
+
+        baseConfig.apply {
+            if (isUsingAutoTheme) {
+                val isUsingSystemDarkTheme = isUsingSystemDarkTheme()
+                isUsingSharedTheme = false
+                textColor =
+                    resources.getColor(if (isUsingSystemDarkTheme) R.color.theme_dark_text_color else R.color.theme_light_text_color)
+                backgroundColor =
+                    resources.getColor(if (isUsingSystemDarkTheme) R.color.theme_dark_background_color else R.color.theme_light_background_color)
+            }
+        }
+
+        if (!baseConfig.isUsingAutoTheme && !baseConfig.isUsingSystemTheme && isThankYouInstalled()) {
+            getSharedTheme {
+                if (it != null) {
+                    baseConfig.apply {
+                        wasSharedThemeForced = true
+                        isUsingSharedTheme = true
+                        wasSharedThemeEverActivated = true
+
+                        textColor = it.textColor
+                        backgroundColor = it.backgroundColor
+                        primaryColor = it.primaryColor
+                        accentColor = it.accentColor
+                    }
+
+                    if (baseConfig.appIconColor != it.appIconColor) {
+                        baseConfig.appIconColor = it.appIconColor
+                        checkAppIconColor()
+                    }
+                }
+                initActivity()
+            }
+        } else {
+            initActivity()
+        }
     }
 }
